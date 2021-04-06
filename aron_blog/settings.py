e3e7004abb29c809b3fed5fe29d42e7f2903b3fb
arunas445 <skittlessxit@gmail.com>
2021-04-07 01:10:55 +0300

    deploy

--- aron_blog/settings.py
+++ aron_blog/settings.py
@@ -160,7 +160,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 AWS_S3_REGION_NAME = 'eu-central-1'
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #AWS_S3_SECURE_URLS = False
 
 
